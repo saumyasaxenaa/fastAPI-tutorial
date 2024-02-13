364e892892364b58dfb25c52102431f4c1f3785c
saumyasaxenaa <saumyasaxena004@gmail.com>
2024-02-13 11:42:54 -0800

    clean up

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI() # create an instance of fastapi
 
@@ -9,3 +11,9 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_post(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
+# title: str, content: str
